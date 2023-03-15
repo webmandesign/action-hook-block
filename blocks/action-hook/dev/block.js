a28a6bf708f35c6b90e6c71b1b80eb3bd619246d
WebMan Design, Oliver Juhas <webmandesigneu@gmail.com>
2023-03-15 14:29:56 +0100

    1.0.0

--- blocks/action-hook/dev/block.js
+++ blocks/action-hook/dev/block.js
@@ -29,41 +29,15 @@
 			return Element(
 
 				/**
-				 * Preview container.
+				 * Preview wrapper container.
 				 */
 				'div',
 				Editor.useBlockProps( {
 					className: ( name ) ? ( 'has-hook-selected' ) : ( '' ),
-        } ),
-
-        /**
-         * "More options" sidebar.
-         */
-				Element( Editor.InspectorControls, {},
-					Element( Comp.Panel, {},
-						Element( Comp.PanelBody, {},
-							Element( Comp.SelectControl,
-								{
-									label    : __( 'Hook name', 'action-hook-block' ),
-									value    : name,
-									options  : hooks,
-									onChange : ( newValue ) => props.setAttributes( { name: newValue } ),
-								}
-							),
-							Element( Comp.TextControl,
-								{
-									label    : __( 'Optional context', 'action-hook-block' ),
-									help     : __( 'Context value will be passed into the action hook as an additional argument.', 'action-hook-block' ),
-									value    : context,
-									onChange : ( newValue ) => props.setAttributes( { context: newValue } ),
-								}
-							),
-						),
-					),
-				),
+				} ),
 
 				/**
-				 * Preview inner HTML.
+				 * Preview inner elements.
 				 */
 
 					// Additional info at top.
@@ -76,7 +50,31 @@
 					( name ) ? ( Element( 'code', {}, "do_action( '" + name + "'" + codeContext + " );" ) ) : ( '' ),
 
 					// Additional info at bottom.
-					( info.bottom ) ? ( Element( 'p', { class: 'wp-block-wmd-action-hook-block__info--bottom' }, info.bottom ) ) : ( '' )
+					( info.bottom ) ? ( Element( 'p', { class: 'wp-block-wmd-action-hook-block__info--bottom' }, info.bottom ) ) : ( '' ),
+
+				/**
+				 * Block settings sidebar.
+				 */
+				Element( Editor.InspectorControls, {},
+					Element( Comp.PanelBody, {},
+						Element( Comp.SelectControl,
+							{
+								label    : __( 'Hook name', 'action-hook-block' ),
+								value    : name,
+								options  : hooks,
+								onChange : ( newValue ) => props.setAttributes( { name: newValue } ),
+							}
+						),
+						Element( Comp.TextControl,
+							{
+								label    : __( 'Optional context', 'action-hook-block' ),
+								help     : __( 'Context value will be passed into the action hook as an additional argument.', 'action-hook-block' ),
+								value    : context,
+								onChange : ( newValue ) => props.setAttributes( { context: newValue } ),
+							}
+						),
+					),
+				)
 
 			);
 		},
